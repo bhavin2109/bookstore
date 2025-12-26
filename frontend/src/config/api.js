f5a603da5f64b2dab684ed8a693695a06fd357a3
bhavin2109 <opbhavin21@gmail.com>
2025-12-26 17:44:49 +0530

    feat: Configure API URL based on environment.

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,4 +2,4 @@
 export const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : import.meta.env.VITE_API_URL || "http://localhost:5000";
+    : import.meta.env.VITE_API_URL || "https://bookstore-rnnf.onrender.com";
