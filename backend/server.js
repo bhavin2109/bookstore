2017f1b38771e04ab07546301ddfc0172e4eb26a
bhavin2109 <opbhavin21@gmail.com>
2025-12-26 16:58:57 +0530

    feat: Initialize backend server with Express, routes, and DB connection, and configure frontend API URL.

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(cors({
    origin: [
     "http://localhost:5173",           // local dev
-    "bookstore-teal-one.vercel.app" // vercel prod
+    "https://bookstore-teal-one.vercel.app" // vercel prod
   ],
   credentials: true
 }));
